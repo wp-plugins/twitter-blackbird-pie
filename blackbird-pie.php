824467118a0e08a61ebd84a51e187d67ea32da49
bradvin <bradvin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-18 18:12:56 +0000

    upgrading to ver 0.2
    git-svn-id: https://plugins.svn.wordpress.org/twitter-blackbird-pie/tags/0.2@242868 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ blackbird-pie.php
@@ -0,0 +1,180 @@
+<?php
+/*
+Plugin Name: Blackbird Pie
+Plugin URI: http://themergency.com
+Description: Add tweet visualizations to your site as can be found at http://media.twitter.com/blackbird-pie/
+Version: 0.1.5
+Author: Brad Vincent
+Author URI: http://themergency.com
+License: GPL2
+*/
+
+class BlackbirdPie {
+	
+	//constructor
+	function BlackbirdPie() {
+		if (!is_admin()) {
+			add_shortcode("blackbirdpie", array(&$this, "shortcode"));
+		}
+	}
+	
+	/*
+	modified from http://www.php.net/manual/en/function.time.php#96097
+	*/
+    function ago($datefrom, $format)
+    {
+        $dateto = time();
+        
+        // Calculate the difference in seconds betweeen
+        // the two timestamps
+
+        $difference = $dateto - $datefrom;
+
+        // Based on the interval, determine the
+        // number of units between the two dates
+        // From this point on, you would be hard
+        // pushed telling the difference between
+        // this function and DateDiff. If the $datediff
+        // returned is 1, be sure to return the singular
+        // of the unit, e.g. 'day' rather 'days'
+    
+        switch(true)
+        {
+            // If difference is less than 60 seconds,
+            // seconds is a good interval of choice
+            case(strtotime('-1 min', $dateto) < $datefrom):
+                $datediff = $difference;
+                $res = 'less than a minute ago';
+                break;
+            // If difference is between 60 seconds and
+            // 60 minutes, minutes is a good interval
+            case(strtotime('-1 hour', $dateto) < $datefrom):
+                $datediff = floor($difference / 60);
+                $res = ($datediff==1) ? $datediff.' minute ago' : $datediff.' minutes ago';
+                break;
+            // If difference is between 1 hour and 24 hours
+            // hours is a good interval
+            case(strtotime('-1 day', $dateto) < $datefrom):
+                $datediff = floor($difference / 60 / 60);
+                $res = ($datediff==1) ? 'about '.$datediff.' hour ago' : 'about '.$datediff.' hours ago';
+                break;				
+            default:
+				$res = date($format, $datefrom);
+        }
+        return $res;
+	}
+	
+	function shortcode($atts) {
+		// Extract the attributes
+		extract(shortcode_atts(array(
+			"id" => false,
+			"url" => false
+		), $atts));
+		
+		if ($url) {
+			if (preg_match('/^http:\/\/twitter\.com\/(\w+)\/status(es)*\/(\d+)$/', $url, $matches)) {
+				$id = $matches[3];
+			}
+		} 
+		
+		if ($id) {
+		
+			//are inside the loop?
+			global $wp_query;
+			if ($wp_query->in_the_loop) {
+				global $post;
+				$post_id = $post->ID;
+			}
+		
+			if ($post_id > 0)
+			{
+				//try and see if we have the blackbird status HTML already saved
+				$blackbird = get_post_meta($post_id, 'bbp_status_'.$id, true);
+				if (!empty($blackbird))
+					return $blackbird;
+			}
+		
+			$data = $this->get_tweet_details($id);
+			$http_code = $data->status->http_code;
+
+			if ($http_code == "200") {
+				require_once('Autolink.php');
+				$autolinker = new Twitter_Autolink();
+				$screenName = $data->contents->user->screen_name;
+				$realName = $data->contents->user->name;
+				$tweetText = $autolinker->autolink($data->contents->text);
+				$source = $data->contents->source;
+				$profilePic = $data->contents->user->profile_image_url;
+				$profileBackgroundColor = $data->contents->user->profile_background_color;
+				$profileBackgroundTile = $data->contents->user->profile_background_tile;
+				if (!$profileBackgroundTile)
+					$profileBackgroundTileHTML = "no-repeat";
+					
+				$profileBackgroundImage = $data->contents->user->profile_background_image_url;
+				$profileTextColor = $data->contents->user->profile_text_color;
+				$profileLinkColor = $data->contents->user->profile_link_color;
+				$createdAt = $data->contents->created_at;
+				$utcOffset = $data->contents->user->utc_offset;
+
+				$timeStamp = strtotime($createdAt); //.' +'.$utcOffset.' seconds');
+
+				$tweetURL = "http://twitter.com/$screenName/status/$id";
+				
+				$dateFormat = get_option('date_format');
+				if (strlen($dateFormat)==0) $dateFormat = __('F j, Y');
+				$timeFormat = get_option('time_format');
+				if (strlen($timeFormat)==0) $timeFormat = __('g:i a');
+				
+				$dateTimeFormat = $dateFormat.' '.$timeFormat;
+				
+				$friendlyDate = date($dateTimeFormat, $timeStamp);
+				
+				$timeAgo = $this->ago($timeStamp, $dateTimeFormat);
+				
+				//echo '|'.$timeAgo.'|';
+
+				if ($screenName != $realName) {
+					$realNameHTML = "<br/>$realName";
+				}
+				
+				$tweetHTML = "<!-- tweet id : $id -->
+			<style type='text/css'>#bbpBox_$id{background:#$profileBackgroundColor url($profileBackgroundImage) $profileBackgroundTileHTML !important;padding:20px;}#bbpBox_$id p.bbpTweet{background:#fff;padding:10px 12px 10px 12px !important;margin:0 !important;min-height:48px;color:#$profileTextColor !important;font-size:18px !important;line-height:22px;-moz-border-radius:5px;-webkit-border-radius:5px}#bbpBox_$id p.bbpTweet a {color:#$profileLinkColor !important}#bbpBox_$id p.bbpTweet span.metadata{display:block;width:100%;clear:both;margin-top:8px  !important;padding-top:12px !important;height:40px;border-top:1px solid #e6e6e6}#bbpBox_$id p.bbpTweet span.metadata span.author{line-height:19px}#bbpBox_$id p.bbpTweet span.metadata span.author img{float:left;margin:0 7px 0px 0px !important;width:38px;height:38px;padding:0 !important;border:none !important;}#bbpBox_$id p.bbpTweet a:hover{text-decoration:underline}#bbpBox_$id p.bbpTweet span.timestamp{font-size:12px;display:block}</style>
+			 
+			<div id='bbpBox_$id'><p class='bbpTweet'>$tweetText<span class='timestamp'><a title='tweeted on $friendlyDate' href='$tweetURL'>$timeAgo</a> via $source</span><span class='metadata'><span class='author'><a href='http://twitter.com/$screenName'><img src='$profilePic' /></a><strong><a href='http://twitter.com/$screenName'>$screenName</a></strong>$realNameHTML</span></span></p></div>
+			<!-- end of tweet -->";
+
+				// save the tweet HTML into a custom field
+				if (!empty($tweetHTML) && $post_id > 0)
+					update_post_meta($post_id, 'bbp_status_'.$id, $tweetHTML);
+
+				return $tweetHTML;
+			} else {
+				return "There was a problem connecting to Twitter.";
+			}
+		}
+		
+		return NULL;
+	}
+
+	function get_tweet_details($id) {
+		
+		$encoded = urlencode("http://api.twitter.com/1/statuses/show.json?id={$id}");
+		
+		$request_url = "http://media.twitter.com/tweetproxy/?url={$encoded}";
+		
+		if(!class_exists('WP_Http'))
+			include_once(ABSPATH . WPINC . '/class-http.php');
+			
+		$request = new WP_Http;
+		$result = $request->request($request_url);
+		
+		if (gettype($result) == "object" && get_class($result) == "WP_Error")
+			return NULL;
+		
+		return json_decode($result["body"]);
+	}
+}
+
+add_action("init", create_function('', 'global $BlackbirdPie; $BlackbirdPie = new BlackbirdPie();'));
+
+?>
---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== Twitter Blackbird Pie ===
+Contributors: bradvin
+Donate link: http://themergency.com/twitter-blackbird-pie-wordpress-plugin/
+Tags: twitter, blackbird pie
+Requires at least: 2.9.2
+Tested up to: 2.9.2
+Stable tag: 0.2
+Version: 0.2
+
+Add awesome looking embedded HTML representations of actual tweets in your blog posts just by adding simple shortcodes.
+
+== Description ==
+
+Add awesome looking embedded HTML representations of actual tweets in your blog posts just by adding simple shortcodes. Please read the blog post at http://themergency.com/twitter-blackbird-pie-wordpress-plugin/ for more info and to see the plugin in action.
+
+The plugins has the following features:
+
+*   Exact same look and feel as the respective Twitter profile.
+*   Allows for multiple "pies" in a single post (as seen in the examples below).
+*   Stores the generated HTML in a custom field (if possible), so the Twitter API is only called the first time.
+*   Slightly better styling than the original Blackbird Pie
+    *   Better use of the Twitter profile background image and color and tiling.
+    *   Uses the Twitter profile text color.
+    *   Uses the Twitter profile link color.
+*   Dates are displayed like on Twitter i.e. "real time" datetime of when the tweet was tweeted.
+*   Auto-linking of URLs, hashtags, usernames within the tweet text.
+*   Use either the id or full URL of the tweet.
+
+== Installation ==
+
+1. Upload the plugin folder 'twitter-blackbird-pie' to your `/wp-content/plugins/` folder
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Insert shortcodes into your pages or posts e.g. [blackbirdpie id="13794126295"] or [blackbirdpie url="http://twitter.com/themergency/status/13968912427"]
+
+== Screenshots ==
+
+1. Example output. See it in action at http://themergency.com/twitter-blackbird-pie-wordpress-plugin/
+
+== Changelog ==
+
+= 0.1 =
+* Initial Relase. First version.
+
+= 0.1.5 =
+* Updated the CSS incl. adding a few "!important" rules to make sure the theme CSS does not override it.
+* Fixed bug for profile background image tile not working.
+* Fixed bug for the date or the tweet. It now takes into account the timezone.
+
+= 0.2 =
+* Removed dependency on Jquery TimeAgo plugin and using a php function instead
+* PLEASE UPGRADE!
+
+== Frequently Asked Questions ==
+
+= How do I use this plugin? =
+You insert shortcodes into your blog posts or pages, e.g. [blackbirdpie id="13794126295"]
+
+== Upgrade Notice ==
+
+There is no upgrade notice
